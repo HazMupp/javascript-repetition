a231b9527a361ca6ec974adae29775fa25ea668f
HazMupp <peter_heier@hotmail.com>
2016-05-02 13:26:58 +0200

    Add some examples

--- repetition.js
+++ repetition.js
@@ -19,4 +19,44 @@ var weekdays = ["måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag", "
 console.log("Exempel 1. Första veckodagen " + weekday[0]);
 
 // Spara i en variabel
-var favoriteDay = weekday[4];
+var favoriteDay = weekdays[4];
+console.log("Exempel 1. Första veckodagen " + favoriteDay);
+
+
+
+var months = [];
+months[0] = "januari";
+months[1] = "februari";
+months[2] = "mars";
+months[3] = "april";
+months[4] = "maj";
+months[5] = "juni";
+months[6] = "juli";
+months[7] = "augusti";
+months[8] = "september";
+months[9] = "oktober";
+months[10] = "november";
+months[11] = "december";
+
+
+// Exempel 3
+// En array kan innehålla flera olika typer (till skillnad från java)
+var favoriteThings = [1997, "Någonting", true, "IT", 1337];
+
+// Exempel 4
+// Hur mycket finns det i en array
+console.log("Exempel 4: Jag har " + favoriteThings.lenght + " favoritsaker");
+
+// Exempel 5
+// Lägg till något på slutet av en array
+favoriteThings.push("Teaching");
+
+// Exempel 6
+// Visa en array
+console.log(favoriteThings);
+
+
+
+
+
+
