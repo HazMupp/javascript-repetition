37992c609d8d41f460dd88c12e0d324a2bb06b23
HazMupp <peter_heier@hotmail.com>
2016-05-02 13:15:24 +0200

    Add array information

---
+++ repetition.js
@@ -0,0 +1,22 @@
+// JavaScript
+
+/*
+Array
+*/
+
+// En array är...
+// Ett ställe där man lagrar
+// En samling med saker
+// En lista
+
+// Exempel 1
+// En första array (metod 1)
+// Vi initierar en array (berättar vad som finns i den samtidigt som den skapas
+var weekdays = ["måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag", "söndag"];
+
+// Hämta ur vår array på följande sätt
+// Skriva ut första veckodagen
+console.log("Exempel 1. Första veckodagen " + weekday[0]);
+
+// Spara i en variabel
+var favoriteDay = weekday[4];
